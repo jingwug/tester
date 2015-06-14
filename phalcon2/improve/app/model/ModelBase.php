b1ecdfe8e1fde4b850a29a929b2a00d4853c30a8
git <git@caimiao.com>
2015-06-15 02:53:35 +0800

    调整模型基类，保证数据保存时，不受默认空值的影响

--- phalcon2/improve/app/model/ModelBase.php
+++ phalcon2/improve/app/model/ModelBase.php
@@ -1,16 +1,89 @@
 <?php
+/**
+ * 模型基类
+ * @author lideqiang
+ */
+class ModelBase extends \Phalcon\Mvc\Model {
 
-use \Phalcon\Mvc\Model;
-
-class ModelBase extends Model
-{
+	/**
+	 * 表前缀
+	 * @var type 
+	 */
 	protected $tablePrefix = 'ys_';
+
+	/**
+	 * 字段默认值
+	 * @var type 
+	 */
+	protected $fieldDefaults = array();
 
 	public function initialize() {
 		$this->setWriteConnectionService('db');
 		$this->setReadConnectionService('db_read');
 		$table = $this->tablePrefix.$this->getSource();
 		$this->setSource($table);
+
+		//取消某项功能，下面这些功能默认为开启的，可关闭
+		$this->setup(array(
+//			'events' => false,				//事件
+//			'columnRenaming' => false,		//字段重命名
+			'notNullValidations'=> false,	//验证字段非空
+//			'virtualForeignKeys' => false,	//验证外键
+//			'phqlLiterals' => false,		//关闭自带的SQL分析器 phql
+		));
+
+		//初始化字段默认值
+		$this->initFieldDefault();
+	}
+
+	/**
+	 * 初始化字段的默认值
+	 * @return \ModelBase
+	 */
+	protected function initFieldDefault() {
+		$fields = $this->toArray();
+		foreach($fields as $field => $value) {
+			$this->fieldDefaults[$field] = '';
+		}
+		return true;
+	}
+
+	/**
+	 * 重置字段NULL值
+	 * @return \ModelBase
+	 */
+	protected function resetFieldNull() {
+		$fileds = $this->toArray();
+		foreach($fileds as $field => $value) {
+			if($value == NULL) {
+				$this->$field = $this->fieldDefaults[$field];
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 保存数据之前
+	 * @return type
+	 */
+	public function beforeSave() {
+		return $this->resetFieldNull();
+	}
+
+	/**
+	 * 创建数据之前
+	 * @return type
+	 */
+	public function beforeCreate() {
+		return $this->resetFieldNull();
+	}
+
+	/**
+	 * 更新数据之前
+	 * @return type
+	 */
+	public function beforeUpdate() {
+		return $this->resetFieldNull();
 	}
 
 }
