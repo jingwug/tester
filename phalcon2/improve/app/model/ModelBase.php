8f743d14a0084515a646179461b661d02750f170
git <git@caimiao.com>
2015-06-15 23:06:55 +0800

    修正ModelBase类，保存数据时的异常

--- phalcon2/improve/app/model/ModelBase.php
+++ phalcon2/improve/app/model/ModelBase.php
@@ -11,12 +11,6 @@ class ModelBase extends \Phalcon\Mvc\Model {
 	 */
 	protected $tablePrefix = 'ys_';
 
-	/**
-	 * 字段默认值
-	 * @var type 
-	 */
-	protected $fieldDefaults = array();
-
 	public function initialize() {
 		$this->setWriteConnectionService('db');
 		$this->setReadConnectionService('db_read');
@@ -31,21 +25,6 @@ class ModelBase extends \Phalcon\Mvc\Model {
 //			'virtualForeignKeys' => false,	//验证外键
 //			'phqlLiterals' => false,		//关闭自带的SQL分析器 phql
 		));
-
-		//初始化字段默认值
-		$this->initFieldDefault();
-	}
-
-	/**
-	 * 初始化字段的默认值
-	 * @return \ModelBase
-	 */
-	protected function initFieldDefault() {
-		$fields = $this->toArray();
-		foreach($fields as $field => $value) {
-			$this->fieldDefaults[$field] = '';
-		}
-		return true;
 	}
 
 	/**
@@ -56,7 +35,7 @@ class ModelBase extends \Phalcon\Mvc\Model {
 		$fileds = $this->toArray();
 		foreach($fileds as $field => $value) {
 			if($value == NULL) {
-				$this->$field = $this->fieldDefaults[$field];
+				$this->$field = '';
 			}
 		}
 		return true;
